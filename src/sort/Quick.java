8abba48e124d5e852b5daa2f3d63f355554238ec
bruce <lilong01@hecom.cn>
2017-03-31 16:59:09 +0800

    三相切分

--- src/sort/Quick.java
+++ src/sort/Quick.java
@@ -1,15 +1,23 @@
 package sort;
 
 import java.util.Arrays;
+import util.StdRandom;
 
 /**
  * Created by Bruce on 2/28/2017.
  */
 public class Quick {
+    public static int swapCount = 0;
+    public static int compareCount = 0;
     public static void main(String[] args){
-        int[] a = {1,31,2,32,3,33,4,56,7,88,9,100};
-        sort3way(a,0,a.length-1);
+        int[] a = {9,9,9,9,9,9,9,9,9,9,31,31,31,31,9,9,9,9,9,9,100,100,100,100,100,100,99,99,99,99,99,99,99,100};
+        StdRandom.shuffle(a);
         System.out.println(Arrays.toString(a));
+        sort3way(a,0,a.length-1);
+//        sort(a, 0, a.length - 1);
+        System.out.println(Arrays.toString(a));
+        System.out.println(swapCount);
+        System.out.println(compareCount);
     }
 
     public static int partition(int[] a, int low, int high){
@@ -17,16 +25,18 @@ public class Quick {
         int j = high+1;
         int v = a[low];
         while(true){
-            while(a[++i] < v) if(i==high) break;
-            while(a[--j] > v) if(j==low) break;
+            while(a[++i] < v) {compareCount++;if(i==high) break;}
+            while(a[--j] > v) {compareCount++;if(j==low) break;}
             if(i>=j) break;
             int tmp = a[j];
             a[j] = a[i];
             a[i] = tmp;
+            swapCount++;
         }
         int tmp = a[j];
         a[j] = a[low];
         a[low] = tmp;
+        swapCount++;
         return j;
     }
 
@@ -75,7 +85,7 @@ public class Quick {
     public static void sort(Double[] a) {
         sort(a, 0, a.length-1);
     }
-    public static void sort3way(int[] a, int low, int high) {
+    public static void sort3way(int[] a, int low, int high) {//三相切分快速排序，非常适合元素种类不多，而且大量重复的数组。
         if(low >= high) return;
         int lt = low, i = low + 1, gt = high;
         int v = a[low];
@@ -85,16 +95,20 @@ public class Quick {
                 a[lt] = a[i];
                 a[i] = tmp;
                 lt++;i++;
+                swapCount++;
             } else if( a[i] > v ) {
                 int tmp = a[gt];
                 a[gt] = a[i];
                 a[i] = tmp;
                 gt--;
+                swapCount++;
             } else {
+//                System.out.println("123");
                 i++;
             }
+            compareCount++;
         }
-        sort(a, low, lt - 1);
-        sort(a, gt + 1, high);
+        sort3way(a, low, lt - 1);
+        sort3way(a, gt + 1, high);
     }
 }
